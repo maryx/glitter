64f4948e202cdf5b061a24724cd274907c151e4e
Mary Xia <maryxia@google.com>
2019-07-23 16:20:09 -0700

    finish effects

--- lib/main.dart
+++ lib/main.dart
@@ -2,56 +2,61 @@ import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(GlitterApp());
 
 const _width = 300.0;
 const _height = 500.0;
 const _durations = <Duration>[
-  Duration(milliseconds: 100),
   Duration(milliseconds: 200),
   Duration(milliseconds: 300),
-  Duration(milliseconds: 400),
   Duration(milliseconds: 500),
-  Duration(milliseconds: 1000)
+  Duration(milliseconds: 700),
+  Duration(milliseconds: 1000),
+  Duration(milliseconds: 1500),
+  Duration(milliseconds: 1700),
+  Duration(milliseconds: 2000),
 ];
 final _random = math.Random();
+final _randomPieceSize = () => _random.nextInt(4).toDouble() + 4;
+final _randomCornerSize = () => _random.nextInt(3).toDouble();
+final _randomDecoration = () => BoxDecoration(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(_randomCornerSize()),
+        topRight: Radius.circular(_randomCornerSize()),
+        bottomLeft: Radius.circular(_randomCornerSize()),
+        bottomRight: Radius.circular(_randomCornerSize()),
+      ),
+    );
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class GlitterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(),
+      title: 'Glitter',
+      home: Glitter(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+/// Glitter modeled off of this gif: https://giphy.com/gifs/pink-5K5oXc0CD7ghG
+class Glitter extends StatefulWidget {
+  Glitter({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _GlitterState createState() => _GlitterState();
 }
 
-class _MyHomePageState extends State<MyHomePage>
-    with TickerProviderStateMixin {
+class _GlitterState extends State<Glitter> with TickerProviderStateMixin {
   final _controllers = <AnimationController>[];
 
   //Animation _animation;
 
-  Color _getRandomColor() {
-    return Color.fromARGB(
-        255, _random.nextInt(255), _random.nextInt(255), _random.nextInt(255));
+  /// Gets pseudo-random color (leans more purple, less dark-green).
+  ///
+  /// Can also use a predefined palette of colors.
+  Color _getRandomColor([int alpha]) {
+    return Color.fromARGB(alpha ?? 255, _random.nextInt(155) + 100,
+        _random.nextInt(255), _random.nextInt(150) + 55);
   }
 
   @override
@@ -59,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage>
     super.initState();
     _durations.forEach((Duration d) {
       _controllers.add(AnimationController(
-        vsync: this, // the SingleTickerProviderStateMixin
+        vsync: this,
         duration: d,
       ));
     });
@@ -76,61 +81,100 @@ class _MyHomePageState extends State<MyHomePage>
     }
   }
 
-//  @override
-//  void didUpdateWidget(Foo oldWidget) {
-//    super.didUpdateWidget(oldWidget);
-//    _controller.duration = _duration;
-//  }
-
   @override
   void dispose() {
     _controllers.forEach((AnimationController c) => c.dispose());
     super.dispose();
   }
 
+  /// This is just playing around with the background.
+//  Widget _createBackgroundAnimation() {
+//    final customController = _controllers[_random.nextInt(_durations.length)];
+//
+//    final colorTween =
+//        ColorTween(begin: _getRandomColor(), end: Colors.transparent);
+//    final tweenAnimation = colorTween.animate(customController);
+//
+//    final positionAnimation =
+//        CurveTween(curve: ElasticInCurve()).animate(customController);
+//
+//    final pieceSize = _random.nextInt(4).toDouble() + 4;
+//
+//    final topOffset = _random.nextDouble() * _height - pieceSize;
+//    final leftOffset = _random.nextDouble() * _width - pieceSize;
+//
+//    return AnimatedBuilder(
+//      animation: customController,
+//      builder: (BuildContext context, Widget widget) {
+//        final piece = Container(
+//            width: pieceSize, height: pieceSize, color: tweenAnimation.value);
+//        //final randomOffset = () { return (positionAnimation.value * _random.nextInt(30) - 15)
+//        return Positioned(
+//          top: topOffset + (positionAnimation.value * _random.nextInt(30) - 15),
+//          left:
+//              leftOffset + (positionAnimation.value * _random.nextInt(30) - 15),
+//          bottom: (positionAnimation.value * _random.nextInt(15)),
+//          right: (positionAnimation.value * _random.nextInt(15)),
+//          child: piece,
+//        );
+//      },
+//    );
+//  }
+
+  /// Glitter piece that animates between opaque and transparent, and also
+  /// moves.
   Widget _createAnimatedGlitterPiece() {
     final customController = _controllers[_random.nextInt(5)];
 
     final colorTween =
-    ColorTween(begin: _getRandomColor(), end: Colors.transparent);
+        ColorTween(begin: _getRandomColor(), end: Colors.transparent);
     final tweenAnimation = colorTween.animate(customController);
 
     final positionAnimation =
-    CurveTween(curve: ElasticInCurve()).animate(customController);
+        CurveTween(curve: ElasticInCurve()).animate(customController);
 
-    final pieceSize = 4.0;
+    final pieceSize = _randomPieceSize();
 
     final topOffset = _random.nextDouble() * (_height - pieceSize);
     final leftOffset = _random.nextDouble() * (_width - pieceSize);
-
 
     return AnimatedBuilder(
       animation: customController,
       builder: (BuildContext context, Widget widget) {
         final piece = Container(
-            width: pieceSize, height: pieceSize, color: tweenAnimation.value);
+          width: pieceSize,
+          height: pieceSize,
+          decoration: _randomDecoration().copyWith(color: tweenAnimation.value),
+        );
 
         return Positioned(
-          top: topOffset + (positionAnimation.value * _random.nextInt(15)),
-          left: leftOffset + (positionAnimation.value * _random.nextInt(15)),
+          top: topOffset + (positionAnimation.value * _random.nextInt(30) - 15),
+          left:
+              leftOffset + (positionAnimation.value * _random.nextInt(30) - 15),
+          //          bottom: (positionAnimation.value * _random.nextInt(15)),
+//          right: (positionAnimation.value * _random.nextInt(15)),
           child: piece,
         );
       },
     );
   }
 
+  /// Glitter piece that animates between transparent and opaque.
   Widget _createReverseAnimatedGlitterPiece() {
     final customController = _controllers[_random.nextInt(5)];
 
     final colorTween =
-    ColorTween(begin: Colors.transparent, end: _getRandomColor());
+        ColorTween(begin: Colors.transparent, end: _getRandomColor());
     final animation = colorTween.animate(customController);
-    final pieceSize = 5.0;
+    final pieceSize = _randomPieceSize();
     final piece = AnimatedBuilder(
       animation: animation,
       builder: (BuildContext context, Widget widget) {
         return Container(
-            width: pieceSize, height: pieceSize, color: animation.value);
+          decoration: _randomDecoration().copyWith(color: animation.value),
+          width: pieceSize,
+          height: pieceSize,
+        );
       },
     );
     final topOffset = _random.nextDouble() * (_height - pieceSize);
@@ -143,12 +187,69 @@ class _MyHomePageState extends State<MyHomePage>
     );
   }
 
+  /// Static glitter piece.
   Widget _createStaticGlitterPiece() {
-    final pieceSize = 7.0;
+    final pieceSize = _randomPieceSize();
     final piece = Container(
-        width: pieceSize, height: pieceSize, color: _getRandomColor());
-    final topOffset = _random.nextDouble() * (_height - pieceSize);
-    final leftOffset = _random.nextDouble() * (_width - pieceSize);
+      decoration: _randomDecoration().copyWith(color: _getRandomColor(170)),
+      width: pieceSize,
+      height: pieceSize,
+    );
+    final topOffset = _random.nextDouble() * _height;
+    final leftOffset = _random.nextDouble() * _width;
+
+    return Positioned(
+      top: topOffset,
+      left: leftOffset,
+      child: piece,
+    );
+  }
+
+  /// T-shaped sparkle.
+  Widget _createSparkle() {
+    final customController = _controllers[_random.nextInt(_durations.length)];
+    final colorTween = ColorTween(begin: Colors.transparent, end: Colors.white);
+    final animation = colorTween.animate(customController);
+    final sparkleSize = _random.nextInt(80).toDouble() + 20;
+    final piece = AnimatedBuilder(
+      animation: animation,
+      builder: (BuildContext context, Widget widget) {
+        final decoration = BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          backgroundBlendMode: BlendMode.colorDodge,
+          gradient: RadialGradient(
+            colors: [Colors.white, Colors.transparent],
+            stops: [-.1, .1],
+            radius: sparkleSize,
+          ),
+          color: animation.value,
+        );
+        final vertical = Container(
+          width: 4,
+          height: sparkleSize,
+          decoration: decoration,
+        );
+        final horizontal = Container(
+          width: sparkleSize,
+          height: 4,
+          decoration: decoration,
+        );
+        return Stack(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: sparkleSize / 2 - 2),
+              child: vertical,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: sparkleSize / 2 - 2),
+              child: horizontal,
+            ),
+          ],
+        );
+      },
+    );
+    final topOffset = _random.nextDouble() * _height;
+    final leftOffset = _random.nextDouble() * _width;
 
     return Positioned(
       top: topOffset,
@@ -159,15 +260,21 @@ class _MyHomePageState extends State<MyHomePage>
 
   List<Widget> _createGlitterPieces(int numPieces) {
     final pieces = <Widget>[];
-//    for (var i = 0; i < numPieces; i++) {
-//      pieces.add(_createStaticGlitterPiece());
+//    for (var i = 0; i < numPieces / 30; i++) {
+//      pieces.add(_createBackgroundAnimation());
 //    }
-    for (var i = 0; i < numPieces; i++) {
+    for (var i = 0; i < numPieces * 2; i++) {
+      pieces.add(_createStaticGlitterPiece());
+    }
+    for (var i = 0; i < numPieces / 8; i++) {
       pieces.add(_createAnimatedGlitterPiece());
     }
-//    for (var i = 0; i < numPieces; i++) {
-//      pieces.add(_createReverseAnimatedGlitterPiece());
-//    }
+    for (var i = 0; i < numPieces; i++) {
+      pieces.add(_createReverseAnimatedGlitterPiece());
+    }
+    for (var i = 0; i < numPieces / 15; i++) {
+      pieces.add(_createSparkle());
+    }
     return pieces;
   }
 
@@ -176,11 +283,12 @@ class _MyHomePageState extends State<MyHomePage>
     return Scaffold(
       body: Center(
           child: Container(
-            height: _height,
-            width: _width,
-            color: Colors.pink[100],
-            child: Stack(children: _createGlitterPieces(1000)),
-          )),
+        height: _height,
+        width: _width,
+        color: Colors.pink[100],
+        // 10000 is a bad idea!
+        child: Stack(children: _createGlitterPieces(600)),
+      )),
     );
   }
 }
